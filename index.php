5c33da8f3e0801ad0e789a760a02c0b1f7fc103d
Gustavo Retamozo <u201416885@upc.edu.pe>
2017-04-29 21:03:04 +0000

    CAMBIOS UNO

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-  <title>Prueba App PHP</title>
+  <title>PRACTICA PHP</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,17 +15,16 @@
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">BUTCHER & SONS</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Menú</a></li>
-        <li><a href="#">Gintonics</a></li>
-        <li><a href="#">Sucursales</a></li>
+        <li><a href="#">Menú 1</a></li>
+        <li><a href="#">mENU 2</a></li>
+        <li><a href="#">mENU 3</a></li>
         
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        
-        <li><a href="#">Menú</a></li>
-        <li><a href="#">Gintonics</a></li>
-        <li><a href="#">Sucursales</a></li>
+        <li><a href="#">Menú 1</a></li>
+        <li><a href="#">mENU 2</a></li>
+        <li><a href="#">mENU 3</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
